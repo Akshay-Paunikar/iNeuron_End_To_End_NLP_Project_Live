8618bd8b1c52a4275a45afb119fd147fdd387488
Akshay Paunikar <akshayakki29@gmail.com>
2024-01-27 19:44:32 +0530

    created custom exception and logging and also tested it successfully

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from textSummarizer.logging import logger
+# from textSummarizer.logging import logger
 
-logger.info("Testing cusom logging for our project")
+# logger.info("Testing cusom logging for our project")
+
+import sys
+from textSummarizer.exception import CustomException
+from textSummarizer.logging import logging
+
+def divide(x:int, y:int) -> int:
+    try:
+        output = x / y
+        return output
+        logging.info("output run successfully")
+        
+    except Exception as e:
+        raise CustomException(e,sys)
+        logging.info("error in calculations")
+    
+
+divide(4, 0)
+        
 
